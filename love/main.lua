2a3bd92968fd73dc638f7c4a8309a9215ac5dff9
Robert Machmer <robert.machmer@gmail.com>
2016-01-19 08:58:23 +0100

    Update main.lua to LÖVE 0.10.0

--- love/main.lua
+++ love/main.lua
@@ -10,42 +10,21 @@ local showDebug = false;
 -- Local Functions
 -- ------------------------------------------------
 
----
--- Check if the hardware supports certain features.
---
-local function checkSupport()
-    print("\n---- SUPPORTED ---- ");
-    print("Canvas:         " .. tostring(love.graphics.isSupported('canvas')));
-    print("PO2:            " .. tostring(love.graphics.isSupported('npot')));
-    print("Subtractive BM: " .. tostring(love.graphics.isSupported('subtractive')));
-    print("Shaders:        " .. tostring(love.graphics.isSupported('shader')));
-    print("HDR Canvas:     " .. tostring(love.graphics.isSupported('hdrcanvas')));
-    print("Multicanvas:    " .. tostring(love.graphics.isSupported('multicanvas')));
-    print("Mipmaps:        " .. tostring(love.graphics.isSupported('mipmap')));
-    print("DXT:            " .. tostring(love.graphics.isSupported('dxt')));
-    print("BC5:            " .. tostring(love.graphics.isSupported('bc5')));
-    print("SRGB:           " .. tostring(love.graphics.isSupported('srgb')));
-
-    print("\n---- RENDERER  ---- ");
-    local name, version, vendor, device = love.graphics.getRendererInfo()
-    print(string.format("Name: %s \nVersion: %s \nVendor: %s \nDevice: %s", name, version, vendor, device));
-end
-
 local function drawStats()
     love.graphics.setColor(100, 100, 100, 255);
-    love.graphics.rectangle('fill', 5, love.window.getHeight() - 185, 200, 200);
+    love.graphics.rectangle('fill', 5, love.graphics.getHeight() - 185, 200, 200);
     love.graphics.setColor(255, 255, 255, 255);
-    love.graphics.print(string.format("FT: %.3f ms", 1000 * love.timer.getAverageDelta()), 10, love.window.getHeight() - 180);
-    love.graphics.print(string.format("FPS: %.3f fps", love.timer.getFPS()), 10, love.window.getHeight() - 160);
-    love.graphics.print(string.format("MEM: %.3f kb", collectgarbage("count")), 10, love.window.getHeight() - 140);
+    love.graphics.print(string.format("FT: %.3f ms", 1000 * love.timer.getAverageDelta()), 10, love.graphics.getHeight() - 180);
+    love.graphics.print(string.format("FPS: %.3f fps", love.timer.getFPS()), 10, love.graphics.getHeight() - 160);
+    love.graphics.print(string.format("MEM: %.3f kb", collectgarbage("count")), 10, love.graphics.getHeight() - 140);
 
     local stats = love.graphics.getStats();
-    love.graphics.print(string.format("Drawcalls: %d", stats.drawcalls), 10, love.window.getHeight() - 120);
-    love.graphics.print(string.format("Canvas Switches: %d", stats.canvasswitches), 10, love.window.getHeight() - 100);
-    love.graphics.print(string.format("TextureMemory: %.2f kb", stats.texturememory / 1024), 10, love.window.getHeight() - 80);
-    love.graphics.print(string.format("Images: %d", stats.images), 10, love.window.getHeight() - 60);
-    love.graphics.print(string.format("Canvases: %d", stats.canvases), 10, love.window.getHeight() - 40);
-    love.graphics.print(string.format("Fonts: %d", stats.fonts), 10, love.window.getHeight() - 20);
+    love.graphics.print(string.format("Drawcalls: %d", stats.drawcalls), 10, love.graphics.getHeight() - 120);
+    love.graphics.print(string.format("Canvas Switches: %d", stats.canvasswitches), 10, love.graphics.getHeight() - 100);
+    love.graphics.print(string.format("TextureMemory: %.2f kb", stats.texturememory / 1024), 10, love.graphics.getHeight() - 80);
+    love.graphics.print(string.format("Images: %d", stats.images), 10, love.graphics.getHeight() - 60);
+    love.graphics.print(string.format("Canvases: %d", stats.canvases), 10, love.graphics.getHeight() - 40);
+    love.graphics.print(string.format("Fonts: %d", stats.fonts), 10, love.graphics.getHeight() - 20);
 end
 
 -- ------------------------------------------------
@@ -57,10 +36,12 @@ function love.load()
     print(string.format("Title: '%s'", getTitle()));
     print(string.format("Version: %.4d", getVersion()));
     print(string.format("LOVE Version: %d.%d.%d (%s)", love.getVersion()));
-    print(string.format("Resolution: %dx%d", love.window.getDimensions()));
+    print(string.format("Resolution: %dx%d", love.graphics.getDimensions()));
 
-    -- Check the user's hardware.
-    checkSupport();
+    print("\n---- RENDERER  ---- ");
+    local name, version, vendor, device = love.graphics.getRendererInfo()
+    print(string.format("Name: %s \nVersion: %s \nVendor: %s \nDevice: %s", name, version, vendor, device));
+
     print("===================")
     print(os.date('%c', os.time()));
     print("===================")
